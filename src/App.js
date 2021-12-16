9a83a03bbe74cc264b4b7b51c6e674a77bfe4021
zataara <bryantmac@gmail.com>
2021-12-16 12:03:19 -0800

    refactored App to use Class based componenets

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import JokeList from "./JokeList";
 
-function App() {
-  return (
-    <div className="App">
-      <JokeList />
-    </div>
-  );
+class App extends React.component {
+
+  render() {
+    return (
+      <div className="App">
+        <JokeList />
+      </div>
+    )
+  }
 }
 
 export default App;
