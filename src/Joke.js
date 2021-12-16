6240fdb190a7d8b67d44800204043b2d97132f69
zataara <bryantmac@gmail.com>
2021-12-16 11:59:41 -0800

    refactored code to use Class Base components

--- src/Joke.js
+++ src/Joke.js
@@ -1,27 +1,47 @@
 import React from "react";
 import "./Joke.css";
 
-function Joke({ vote, votes, text, id }) {
-  const upVote = () => vote(id, +1);
-  const downVote = () => vote(id, -1);
 
-  return (
+class Joke extends React.Component {
+  constructor(props) {
+    super(props);
+    this.upVote = this.upVote.bind(this);
+    this.downVote = this.downVoate.bind(this)
+    this.toggleLock = this.toggleLock.bind(this);
+  }
+
+  upVote() {
+    this.props.vote(this.props.id, +1);
+  }
+
+  downVote() {
+    this.props.vote(this.props.id, -1);
+  }
+
+  toggleLock() {
+    this.props.toggleLock(this.props.id)
+  }
+
+  render() {
+    return(
     <div className="Joke">
       <div className="Joke-votearea">
-        <button onClick={upVote}>
+        <button onClick={this.upVote}>
           <i className="fas fa-thumbs-up" />
         </button>
 
-        <button onClick={downVote}>
+        <button onClick={this.downVote}>
           <i className="fas fa-thumbs-down" />
         </button>
 
-        {votes}
+        {this.props.votes}
       </div>
 
-      <div className="Joke-text">{text}</div>
+      <div className="Joke-text">{this.props.text}</div>
     </div>
-  );
+    )}
 }
+
+
 
 export default Joke;
